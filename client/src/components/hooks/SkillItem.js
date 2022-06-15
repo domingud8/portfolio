d3fdd99befa4f08c66fbcedd634613ffd352df49
domingud8 <ddmguez8@gmail.com>
2022-06-15 10:42:49 +0200

    adding files in public directory

--- client/src/components/hooks/SkillItem.js
+++ client/src/components/hooks/SkillItem.js
@@ -1,5 +1,5 @@
 export default function SkillItem({ text, img_name }) {
-    const path = "./icons8_skills/" + img_name + ".png";
+    const path = img_name + ".png";
     return (
         <div className="skill-item d-flex flex-row justify-content-center align-items-center">
             <div>
