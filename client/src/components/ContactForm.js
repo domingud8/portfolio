0a2b34ced62498266a26827be99c17f7130efcff
domingud8 <ddmguez8@gmail.com>
2022-06-17 10:12:30 +0200

    adding resonsiveness Nav, About sections

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -41,10 +41,8 @@ export default function Contact() {
             onSubmit={handleSubmit}
             style={{ color: "white", fontSize: "22px" }}
         >
-            <div style={{ paddingBottom: "20px" }} className="form-group">
-                <label style={{ paddingBottom: "10px" }}>Contact Me?</label>{" "}
-                <br />
-                <label style={{ paddingBottom: "10px" }}>Name</label>
+            <div style={{ paddingBottom: "20px" }} className="form-group ">
+                <label>Name</label>
                 <input
                     className="form-control"
                     id="name"
